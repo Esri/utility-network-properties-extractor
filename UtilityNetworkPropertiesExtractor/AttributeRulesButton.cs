b89329fd93337db65e0a696fcf5d389aa5f221e3
mhirschhiemer <mhirschheimer@esri.com>
2024-05-12 18:53:18 -0400

    ArcGIS Pro SDK 3.0 SDK for .NET

--- UtilityNetworkPropertiesExtractor/AttributeRulesButton.cs
+++ UtilityNetworkPropertiesExtractor/AttributeRulesButton.cs
@@ -10,11 +10,12 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-using ArcGIS.Core.CIM;
 using ArcGIS.Core.Data;
+using ArcGIS.Core.Data.UtilityNetwork;
 using ArcGIS.Desktop.Core.Geoprocessing;
 using ArcGIS.Desktop.Framework.Contracts;
 using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -32,9 +33,8 @@ namespace UtilityNetworkPropertiesExtractor
             ProgressDialog progDlg = new ProgressDialog("Extracting Attribute Rule CSV(s) to:\n" + Common.ExtractFilePath); ;
 
             try
-            {               
+            {
                 progDlg.Show();
-
                 await ExtractAttributeRulesAsync();
             }
             catch (Exception ex)
@@ -51,103 +51,109 @@ namespace UtilityNetworkPropertiesExtractor
         {
             await QueuedTask.Run(async () =>
             {
-                List<DataSourceInMap> dataSourceInMapList = DataSourcesInMapHelper.GetDataSourcesInMap();
-                foreach (DataSourceInMap dataSourceInMap in dataSourceInMapList)
+                string dateFormatted = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                string attrRuleFileName = string.Empty;
+
+                Common.CreateOutputDirectory();
+                Dictionary<string, Table> tablesDict = new Dictionary<string, Table>();
+
+                //If Subtype Group layers are in the map, will have multiple layers pointing to same source featureclass
+                //Populate Dictionary of distinct featureclasses
+                IEnumerable<FeatureLayer> featureLayerList = MapView.Active.Map.GetLayersAsFlattenedList().OfType<FeatureLayer>();
+                foreach (FeatureLayer featureLayer in featureLayerList)
                 {
-                    if (dataSourceInMap.WorkspaceFactory != WorkspaceFactory.FeatureService.ToString() || dataSourceInMap.WorkspaceFactory != WorkspaceFactory.Shapefile.ToString())
-                    {
-                        using (Geodatabase geodatabase = dataSourceInMap.Geodatabase)
-                        {
-                            // FeatureClasses
-                            string pathToTable;
-                            IReadOnlyList<FeatureClassDefinition> fcDefinitionList = geodatabase.GetDefinitions<FeatureClassDefinition>();
-                            foreach (FeatureClassDefinition fcDefinition in fcDefinitionList)
-                            {
-                                try
-                                {
-                                    //Determine the feature dataset name (if applicable) for the featureclass
-                                    using (FeatureClass featureClass = geodatabase.OpenDataset<FeatureClass>(fcDefinition.GetName()))
-                                    {
-                                        FeatureDataset featureDataset = featureClass.GetFeatureDataset();
-                                        if (featureDataset != null)
-                                        {
-                                            //Use the absolute path for local files results in escape characters for things like spaces, which the GP tool can't handle for input/output of local files.
-                                            pathToTable = $"{geodatabase.GetPath().LocalPath}/{featureDataset.GetName()}/{fcDefinition.GetName()}";
-                                        }
-                                        else
-                                            pathToTable = $"{geodatabase.GetPath().LocalPath}/{fcDefinition.GetName()}";
+                    Table table = Common.GetTableFromFeatureLayer(featureLayer);
+                    string tableName = table.GetName();
 
-
-                                        Console.WriteLine(pathToTable);
-
-                                        //Call GP Tool to Export Attribute Rules
-                                        await CallGPTool(dataSourceInMap, pathToTable, fcDefinition.GetName());
-                                    }
-                                }
-                                catch (Exception ex)
-                                {
-                                    MessageBox.Show(ex.Message, $"Extract Attribute Rules - {fcDefinition.GetName()} ");
-                                }
-                            }
-
-                            // Tables
-                            IReadOnlyList<TableDefinition> tableDefinitionList = geodatabase.GetDefinitions<TableDefinition>();
-                            foreach (TableDefinition tableDefinition in tableDefinitionList)
-                            {
-                                try
-                                {
-                                    using (Table table = geodatabase.OpenDataset<Table>(tableDefinition.GetName()))
-                                    {
-                                        //Use the absolute path for local files results in escape characters for things like spaces, which the GP tool can't handle for input/output of local files.
-                                        pathToTable = $"{geodatabase.GetPath().LocalPath}/{tableDefinition.GetName()}";
-
-                                        //Call GP Tool to Export Attribute Rules
-                                        await CallGPTool(dataSourceInMap, pathToTable, tableDefinition.GetName());
-                                    }
-                                }
-                                catch (Exception ex)
-                                {
-                                    MessageBox.Show(ex.Message, $"Extract Attribute Rules - {tableDefinition.GetName()} ");
-                                }
-                            }
-                        }
-                    }
-
-                    //Delete files were ARs weren't assigned to the object.
-                    DeleteEmptyFiles(dataSourceInMap);
+                    if (!tablesDict.ContainsKey(tableName))
+                        tablesDict.Add(tableName, table);
                 }
+
+                //Standalone Tables
+                IReadOnlyList<StandaloneTable> standaloneTableList = MapView.Active.Map.StandaloneTables;
+                foreach (StandaloneTable standaloneTable in standaloneTableList)
+                {
+                    Table table = standaloneTable.GetTable();
+                    string tableName = table.GetName();
+
+                    if (!tablesDict.ContainsKey(tableName))
+                        tablesDict.Add(tableName, table);
+                }
+
+                //Execute GP for each table in the dictionary
+                foreach (KeyValuePair<string, Table> pair in tablesDict)
+                {
+                    string fcName = pair.Key;
+                    int pos = pair.Key.LastIndexOf(".");
+
+                    if (pos != -1) // strip off schema and owner of Featureclass Name (if exists).  Ex:  meh.unadmin.ElectricDevice
+                        fcName = pair.Key.Substring(pos + 1);
+
+                    //attrRuleFileName = string.Format("{0}_{1}_AttributeRules_{2}.csv", dateFormatted, reportHeaderInfo.MapName, fcName);
+                    //string attrRuleoutputFile = Path.Combine(Common.ExtractFilePath, attrRuleFileName);
+
+                    string attrRuleOutputFile = Common.CreateCsvFile($"AttributeRules_{fcName}", string.Empty);
+                    string pathToTable = pair.Key;
+                    IReadOnlyList<string> attrRuleArgs;
+
+                    using (Datastore datastore = pair.Value.GetDatastore())
+                    {
+                        if (datastore is UnknownDatastore)
+                            continue;
+
+                        Uri uri = datastore.GetPath();
+                        if (uri.AbsoluteUri.ToLower().Contains("https")) // can't extract Attibute Rules when layer's source is a FeatuerService
+                            continue;
+
+                        //Using the absolute path for local files results in escape characters for things like spaces, which the GP tool can't handle for input/output of local files.
+                        var datastorePath = uri.LocalPath;
+
+                        FeatureClass featureclass = pair.Value as FeatureClass;
+                        FeatureDataset featureDataset = null;
+
+                        if (featureclass != null)
+                            featureDataset = featureclass.GetFeatureDataset();
+
+                        if (featureDataset == null)
+                        {
+                            //<path to connfile>.sde/meh.unadmin.featureclass
+                            pathToTable = string.Format("{0}\\{1}", datastorePath, pair.Value.GetName());
+                        }
+                        else
+                        {
+                            //<path to connfile>.sde/meh.unadmin.Electric\meh.unadmin.ElectricDevice
+                            string featureDatasetName = featureclass.GetFeatureDataset().GetName();
+                            pathToTable = string.Format("{0}\\{1}\\{2}", datastorePath, featureDatasetName, pair.Value.GetName());
+                        }
+
+                        //arcpy.management.ExportAttributeRules("DHC Line", r"C:\temp\DHCLine_AR_rules.CSV")
+                        pathToTable = pathToTable.Replace("\\", "/");
+                        attrRuleArgs = Geoprocessing.MakeValueArray(pathToTable, attrRuleOutputFile);
+                        var result = await Geoprocessing.ExecuteToolAsync("management.ExportAttributeRules", attrRuleArgs);
+                    }
+                }
+
+                //Delete files that only have 1 line (header) which means 0 Attribute Rules are assigned
+                DirectoryInfo directoryInfo = new DirectoryInfo(Common.ExtractFilePath);
+                List<FileInfo> blankFiles = directoryInfo.GetFiles().Where(f => f.Extension == ".csv" && f.Name.Contains("_AttributeRules")).ToList();
+                foreach (FileInfo bf in blankFiles)
+                {
+                    string[] lines = File.ReadAllLines(bf.FullName);
+                    int cnt = lines.Count();
+
+                    if (cnt == 1)
+                        bf.Delete();
+                }
+
+                //Delete the .xml files that are genereated by the GP tool
+                List<FileInfo> deleteableFiles = directoryInfo.GetFiles().Where(f => f.Extension == ".xml" && f.Name.Contains("_AttributeRules")).ToList();
+                foreach (FileInfo file in deleteableFiles)
+                    file.Delete();
+
+                FileInfo[] schemaIniFile = directoryInfo.GetFiles("schema.ini");
+                foreach (FileInfo schemaIni in schemaIniFile)
+                    schemaIni.Delete();
             });
         }
-
-        private static async Task CallGPTool(DataSourceInMap dataSourceInMap, string pathToTable, string objectName)
-        {
-            string attrRuleOutputFile = Common.CreateCsvFile($"AttributeRules_{objectName}", dataSourceInMap.NameForCSV);
-            IReadOnlyList<string> attrRuleArgs = Geoprocessing.MakeValueArray(pathToTable, attrRuleOutputFile);
-            await Geoprocessing.ExecuteToolAsync("management.ExportAttributeRules", attrRuleArgs);
-        }
-
-        private static void DeleteEmptyFiles(DataSourceInMap dataSourceInMap)
-        {
-            //Delete files that only have 1 line (header) which means 0 Attribute Rules were assigned
-            DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(Common.ExtractFilePath, dataSourceInMap.NameForCSV));
-            List<FileInfo> blankFiles = directoryInfo.GetFiles().Where(f => f.Extension == ".csv" && f.Name.Contains("_AttributeRules")).ToList();
-            foreach (FileInfo bf in blankFiles)
-            {
-                string[] lines = File.ReadAllLines(bf.FullName);
-                int cnt = lines.Count();
-
-                if (cnt == 1)
-                    bf.Delete();
-            }
-
-            //Delete the .xml files that are genereated by the GP tool
-            List<FileInfo> deleteableFiles = directoryInfo.GetFiles().Where(f => f.Extension == ".xml" && f.Name.Contains("_AttributeRules")).ToList();
-            foreach (FileInfo file in deleteableFiles)
-                file.Delete();
-
-            FileInfo[] schemaIniFile = directoryInfo.GetFiles("schema.ini");
-            foreach (FileInfo schemaIni in schemaIniFile)
-                schemaIni.Delete();
-        }       
     }
 }
